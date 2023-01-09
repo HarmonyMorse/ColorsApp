ec4a8115338450ad7c3a296d47a34f74b3205c91
HarmonyMorse <harmonymorse@icloud.com>
2023-01-09 14:59:44 -0800

    Conformed ColorsTableViewController to UIViewController, UITableViewDelegate, UITableViewDataSource (in extension). Filled in tableView-numberOfRowsInSection (returned 50). Filled in tableView-cellForRowAt (returned empty UITableViewCell).

--- ColorsApp/ColorsApp/ColorsTableViewController.swift
+++ ColorsApp/ColorsApp/ColorsTableViewController.swift
@@ -14,15 +14,17 @@ class ColorsTableViewController: UIViewController {
 
         // Do any additional setup after loading the view.
     }
-    
-    /*
-    // MARK: - Navigation
+}
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+
+extension ColorsTableViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 50
     }
-    */
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
 
 }
