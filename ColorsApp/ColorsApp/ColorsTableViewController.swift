1b6c77d8c00b924042018d6167918ff78cbeba06
HarmonyMorse <harmonymorse@icloud.com>
2023-01-10 15:53:45 -0800

    In ColorsTableViewController class: 	Created a "colors" array of type UIColor, initially is empty. 	Added function (createAllRandomColors) to fill "colors" with 50 random UIColor's. 	In tableView - cellForRowAt: 		Unwrapped "ColorCell" and returned it with the appropriate background color via "colors[indexPath.row]"

--- ColorsApp/ColorsApp/ColorsTableViewController.swift
+++ ColorsApp/ColorsApp/ColorsTableViewController.swift
@@ -8,23 +8,45 @@
 import UIKit
 
 class ColorsTableViewController: UIViewController {
-
+    
+    var colors: [UIColor] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        createAllRandomColors()
         // Do any additional setup after loading the view.
     }
+    
+    func createAllRandomColors() {
+        for _ in 0..<50 {
+            colors.append(createOneRandomColor())
+        }
+    }
+    
+    func createOneRandomColor() -> UIColor {
+        let randomColor = UIColor(red: CGFloat.random(in: 0...1),
+                                  green: CGFloat.random(in: 0...1),
+                                  blue: CGFloat.random(in: 0...1),
+                                  alpha: 1)
+        return randomColor
+    }
+    
 }
 
 
 extension ColorsTableViewController: UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 50
+        return colors.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return UITableViewCell()
+        
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "ColorCell") else { return UITableViewCell() }
+        
+        cell.backgroundColor = colors[indexPath.row]
+        
+        return cell
     }
     
     
